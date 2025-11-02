ef4840f37f7db8152d8cfd469ee8a1183e8d30a9
Jinnawat inyim <i.jinnawat024@gmail.com>
2025-11-02 21:07:23 +0700

    feat(staff):Add staff management feature
    Introduces staff list, detail, and form pages with routing and service for CRUD operations. Updates sidebar navigation and staff model to support staff management functionality.

---
+++ src/app/features/staff/pages/staff-detail/staff-detail.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
+import { StaffService } from '../../services/staff.service';
+import { Staff } from '../../../../shared/models/staff.model';
+
+@Component({
+  selector: 'app-staff-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './staff-detail.page.html',
+  styleUrl: './staff-detail.page.scss'
+})
+export class StaffDetailPage implements OnInit {
+  staff: Staff | undefined;
+
+  constructor(
+    private staffService: StaffService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      if (id) {
+        this.staff = this.staffService.getStaffById(id);
+        if (!this.staff) {
+          this.router.navigate(['/staff']);
+        }
+      }
+    });
+  }
+
+  editStaff(): void {
+    if (this.staff) {
+      this.router.navigate(['/staff', this.staff.id, 'edit']);
+    }
+  }
+
+  goBack(): void {
+    this.router.navigate(['/staff']);
+  }
+}
+
